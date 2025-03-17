c4295af3cb500353158700f6f79e7088a60e6554
Ammar Hammad <a2mar@protonmail.com>
2025-03-17 19:32:19 +0100

    - added dynamic game object hierarchy creation parallel to scripts

--- Assets/BFH/Scripts/BezierStreet/BezierRoadManager.cs
+++ Assets/BFH/Scripts/BezierStreet/BezierRoadManager.cs
@@ -14,22 +14,50 @@ public class BezierRoadManager : MonoBehaviour
 {
     private BezierCurveExample bezierCurve;
     private BezierRoadMesh roadMesh;
+    private GameObject bezierCurveObject;  // GO for curves and lanes data
+    private GameObject roadMeshObject;  // GO for road mesh
+    private GameObject roadSegment;  // GO for road segment
+
 
     void Start()
     {
         // create BezierCurveExample and BezierRoadMesh dynamically
-        bezierCurve = gameObject.AddComponent<BezierCurveExample>();
-        roadMesh = gameObject.AddComponent<BezierRoadMesh>();
-
-        // ensure components exist before proceeding
-        if (bezierCurve == null || roadMesh == null)
-        {
-            Debug.LogError("BezierCurveExample or BezierRoadMesh could not be created!");
-            return;
-        }
+        AssignComponents();
 
         // generate the initial road mesh
         UpdateRoadMesh();
+    }
+
+    void AssignComponents()
+    {
+        // check if the objects already exist, if not, create them
+        if (roadSegment == null)
+        {
+            roadSegment = new GameObject("RoadSegment");
+            roadSegment.transform.parent = this.transform;
+        }
+
+        if (bezierCurveObject == null)
+        {
+            bezierCurveObject = new GameObject("BezierCurveContainer");
+            bezierCurveObject.transform.parent = roadSegment.transform; // set as a child of the manager
+            bezierCurve = bezierCurveObject.AddComponent<BezierCurveExample>();
+        }
+        else
+        {
+            bezierCurve = bezierCurveObject.GetComponent<BezierCurveExample>();
+        }
+
+        if (roadMeshObject == null)
+        {
+            roadMeshObject = new GameObject("RoadMeshContainer");
+            roadMeshObject.transform.parent = roadSegment.transform; // set as a child of the manager
+            roadMesh = roadMeshObject.AddComponent<BezierRoadMesh>();
+        }
+        else
+        {
+            roadMesh = roadMeshObject.GetComponent<BezierRoadMesh>();
+        }
     }
 
     void Update()
