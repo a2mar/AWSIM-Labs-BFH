e396923ceafae47792fee4e5223375495dc7a96a
Ammar Hammad <a2mar@protonmail.com>
2025-03-22 19:12:01 +0100

    WIP new randomization

--- Assets/BFH/Scripts/BezierStreet/BezierRoadManager.cs
+++ Assets/BFH/Scripts/BezierStreet/BezierRoadManager.cs
@@ -9,6 +9,10 @@
 
 using System;
 using UnityEngine;
+using System.Linq;
+
+// using System.Numerics;
+
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -17,7 +21,7 @@ using UnityEditor;
 [ExecuteInEditMode()]
 public class BezierRoadManager : MonoBehaviour
 {
-    private int segmentCount = 8;  // at least 4 for cricle creation
+    private int segmentCount = 32;  // at least 4 for cricle creation
     private float radius = 160.0f;
     private BezierCurveExample[] bezierCurves;  // Bezier Curve Data component
     private BezierRoadMesh[] roadMeshes;  // road mesh component
@@ -31,7 +35,26 @@ public class BezierRoadManager : MonoBehaviour
     // how much the control knots can be randomply scattered
     [Header("Range of Random Scattering of Bezier Control Knots (0 means no random scattering)")]
     public float scatteringRange = 0f;
+    // how likely it is that a certain knot is scatered
+    [Header("Probability of random scattering per knot")]
+    public float randomProb = 100.0f;
+
+    // secondary scattering
+    [Header("Range of Secondary Random Scattering of Bezier Control Knots (0 means no secondary random scattering)")]
+    public float secundaryScatteringRange = 0f;
     private float[] randomNumbers;
+
+    public enum RoadType
+    {
+        Simple,  // randomize every 8th Bezier end control knot
+        Medium,  // randomize every 4th Bezier end control knot
+        Crazy  //  randomize every 2nd Bezier end control knot
+    }
+
+    [Header("Road Type")]
+    public RoadType roadType = RoadType.Simple;
+
+    private int[] primaryScatterPoints;
 
     /// <summary>
     /// TODO: determine, in which mode the component is supposed to be started.
@@ -52,23 +75,31 @@ public class BezierRoadManager : MonoBehaviour
 
     void OnValidate()
     {
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         EditorApplication.delayCall += () =>
         {
             if (this != null)
             {
-                // UPDATE Road with new random scattering
-                // create Vectors for the Bezier curves and update the Bezier Curve Components
-                InitializeBezierKnots();
-                // generate the initial road mesh
-                UpdateRoadMesh();
-
+                // Update the road, including random values, geometric and mesh components
+                UpdateRoad();
             }
         };
-        #endif
+#endif
 
     }
 
+    void UpdateRoad()
+    {
+        // determin which Bezier segments to randomize with primary scattering, according to road type
+        DetermineRandomBezierSegments();
+        // generate deviation factors for random scattering
+        GenerateRandomNumbers();
+        // create Vectors for the Bezier curves and update the Bezier Curve Components
+        InitializeBezierKnots();
+        RandomizeBezierKnots();
+        // generate the initial road mesh
+        UpdateRoadMesh();
+    }
     public void UpdateRoadMesh()
     {
         for (int i = 0; i < segmentCount; i++)
@@ -82,32 +113,56 @@ public class BezierRoadManager : MonoBehaviour
         // allocate the Bezier Knots array
         bezierKnots = new Vector3[segmentCount][];
 
-        // generate deviation factors for random scattering
-        GenerateRandomNumbers();
-
-
         // allocate each row to hold 4 Vector3 elements (cubic Bezier curve's control knots)
         for (int i = 0; i < segmentCount; i++)
         {
             Vector3[] previous = i > 0 ? bezierKnots[i - 1] : null;
             // bezierKnots[i] = CubicBezierKnots(previous);
-            bezierKnots[i] = CircularCubicBezierKnots(i);
+            bezierKnots[i] = CircularCubicBezierKnotsV2(i);
             // apply to the corresponding Bezier Curve Components
             bezierCurves[i].ApplyMainBezierKnots(bezierKnots[i]);
         }
     }
 
+    void DetermineRandomBezierSegments()
+    {
+        // define gap (# non-rand. segments) between two randomized segments 
+        int gap = roadType == RoadType.Simple ?
+        8 : roadType == RoadType.Medium ?
+        4 : 2;
+
+        // calculate how many points have primary randomization
+        int amountOfPoints = (segmentCount / gap) - 1;  // -1 to prevent last segment to have scattering
+        primaryScatterPoints = new int[amountOfPoints];
+        System.Random random = new System.Random();
+        int start = Math.Max((int)(random.NextDouble() * (gap - 1)), 3);
+
+        for (int i = 0; i < amountOfPoints; i++)
+        {
+            primaryScatterPoints[i] = i * gap + start;
+        }
+
+    }
+
     /// <summary>
     /// Generates the random deviations for all road segments except the first
+    /// TODO: adjust and rename according to new functionality 
     /// </summary>
     private void GenerateRandomNumbers()
     {
         randomNumbers = new float[segmentCount];
-        randomNumbers[segmentCount - 1] = 1;
+        // no randomization on the last segment's end point
+        randomNumbers[segmentCount - 1] = 0;
         for (int i = 0; i < segmentCount - 1; i++)
         {
+            // determine if the segment has randomization
+            bool randomize = primaryScatterPoints.Contains(i);
+
             System.Random random = new System.Random();
-            randomNumbers[i] = (float)(random.NextDouble() * 2.0 * scatteringRange - scatteringRange);
+
+            // apply randomization based on scattering range
+            if (!randomize) randomNumbers[i] = 0;
+            else randomNumbers[i] = (float)(random.NextDouble() * 2.0 * scatteringRange - scatteringRange);
         }
     }
 
@@ -167,7 +222,7 @@ public class BezierRoadManager : MonoBehaviour
         // from Stackoverflow: (4/3)*tan(pi/(2n)) (https://stackoverflow.com/questions/1734745/how-to-create-circle-with-b%C3%A9zier-curves)
         float amp = (4.0f / 3.0f) * Mathf.Tan((float)(Math.PI / (2.0f * segmentCount)));
 
-        // C2 CONTINUITY WITH RANDOMNES        
+        // C1 CONTINUITY WITH RANDOMNES        
         // use the previous quadrouple of Bezier knots for calculating the curent knots
         Vector3 p_1;
         if (segment == 0) p_1 = amp * Vector3.Cross(p_0, normal) + p_0;  //  keep the first segment straight for starting
@@ -177,6 +232,70 @@ public class BezierRoadManager : MonoBehaviour
         else p_2 = (amp * Vector3.Cross(normal, p_3) + p_3) * deviation1;  //  1) calculate perfect circle knot 2) multiply with rand. dev.
 
         return new Vector3[] { p_0, p_1, p_2, p_3 };
+    }
+
+    Vector3[] CircularCubicBezierKnotsV2(int segment)
+    {
+        // check if minimum amount of segments is 4
+        if (segmentCount < 4)
+        {
+            Debug.LogError("Two little segments for Cricle creation");
+            return null;
+        }
+
+        // calculate angle between endpoints
+        float angleRadian = (float)(Math.PI * 2 / segmentCount);
+
+        // start points
+        float x_0 = radius * Mathf.Cos(angleRadian * segment);
+        float z_0 = radius * Mathf.Sin(angleRadian * segment);
+        Vector3 p_0 = new(x_0, 0, z_0);
+
+        // end points
+        float x_3 = radius * Mathf.Cos(angleRadian * (segment + 1));
+        float z_3 = radius * Mathf.Sin(angleRadian * (segment + 1));
+        Vector3 p_3 = new(x_3, 0, z_3);
+
+        // CALCULATE THE INTERMEDIARY KNOTS
+        // define normal vector
+        Vector3 normal = new(0, 1, 0);
+        // calculate the amplitude factor for the tangents. 
+        // It is directly related to the number of segments for the circle.
+        // from Stackoverflow: (4/3)*tan(pi/(2n)) (https://stackoverflow.com/questions/1734745/how-to-create-circle-with-b%C3%A9zier-curves)
+        float amp = (4.0f / 3.0f) * Mathf.Tan((float)(Math.PI / (2.0f * segmentCount)));
+
+        // calculate the knots
+        Vector3 p_1 = amp * Vector3.Cross(p_0, normal) + p_0;
+        Vector3 p_2 = amp * Vector3.Cross(normal, p_3) + p_3;
+
+        return new Vector3[] { p_0, p_1, p_2, p_3 };
+    }
+
+    /// <summary>
+    /// Randomize Bezier end knots based on the probability for randomization
+    /// </summary>
+    void RandomizeBezierKnots()
+    {
+        System.Random random = new System.Random();
+
+        for (int i = 0; i < bezierKnots.Length; i++)
+        {
+            // calculate the displacement factor from random deviations
+            float deviation = (100.0f + randomNumbers[i]) / 100.0f;
+
+            bezierKnots[i][3] = bezierKnots[i][3] * deviation;
+
+            bezierKnots[i][0] = bezierKnots[(segmentCount + i - 1) % segmentCount][3];
+            bezierCurves[i].ApplyMainBezierKnots(bezierKnots[i]);
+
+            // 
+        }
+    }
+
+    Vector3[] UpdatedCubicBezierKnots(int segment)
+    {
+
+        return new Vector3[] { };
     }
 
     void AssignComponents()
